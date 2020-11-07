ed3a0a8d4f823dd39f940ec49874aa49921b4b3f
César <cesaramolinero@gmail.com>
2020-11-07 11:38:30 +0100

    cleaning code and scripting

--- app.js
+++ app.js
@@ -5,18 +5,12 @@ const cookieParser  = require('cookie-parser');
 const urllib        = require('url');
 const path          = require('path');
 const crypto        = require('crypto');
-const fs            = require('fs');
 
 const config        = require('./config.json');
 const defaultroutes = require('./routes/default');
 const passwordauth  = require('./routes/password');
 const webuathnauth  = require('./routes/webauthn.js');
 
-var http = require('http');
-var https = require('https');
-var privateKey  = fs.readFileSync('./sslcert/key.pem', 'utf8');
-var certificate = fs.readFileSync('./sslcert/cert.pem', 'utf8');
-var credentials = {key: privateKey, cert: certificate};
 const app           = express();
 
 app.use(bodyParser.json());
@@ -39,14 +33,7 @@ app.use('/password', passwordauth)
 app.use('/webauthn', webuathnauth)
 
 const port = config.port || 3000;
-const portHttps = config.portHttps || 3030
-
-var http = http.createServer(app)
-var httpsServer = https.createServer(credentials, app)
-
-http.listen(port);
-httpsServer.listen(portHttps);
+app.listen(port);
 console.log(`Started app on port ${port}`);
-console.log(`Started app on port ${portHttps}`);
 
 module.exports = app;
